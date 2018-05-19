bb03b6d6f8d3cff4540246d85e09e9a7fe1c0449
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-05-19 06:25:07 +0530

    Create HUNTER11-TO REVERSE EVERY WORD IN STRING.C

---
+++ HUNTER11-TO REVERSE EVERY WORD IN STRING.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main()
+{
+   char a[100];
+   int i,l;
+   scanf("%[^\n]s",a);
+   l=strlen(a);
+   for(i=l-1;i>=0;i--)
+   {
+       printf("%c",a[i]);
+   }
+
+    return 0;
+}
