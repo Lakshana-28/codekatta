79c92360af0a433b144ae827379b9251527879d7
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-05-19 06:22:01 +0530

    Create HUNTER10-TO FIND IF ARRAY B IS SUBSET OF ARRAY A.C

---
+++ HUNTER10-TO FIND IF ARRAY B IS SUBSET OF ARRAY A.C
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a[100],b[100],i,n,m,j,cnt=0;
+   scanf("%d",&n);
+   scanf("%d",&m);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(j=0;j<m;j++)
+   {
+       scanf("%d",&b[j]);
+   }
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<m;j++)
+       {
+           if(a[i]==b[j])
+           {
+               cnt++;
+
+           }
+
+     }
+     if(cnt>=1)
+
+           {
+               printf("yes");
+               break;
+           }
+
+   }
+
+}
