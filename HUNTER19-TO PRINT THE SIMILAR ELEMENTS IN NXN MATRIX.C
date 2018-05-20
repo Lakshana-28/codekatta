adbb20957722acbf0ae27cb1aac7621cb9c0bbbe
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-05-20 08:08:46 +0530

    Create HUNTER19-TO PRINT THE SIMILAR ELEMENTS IN NXN MATRIX.C

---
+++ HUNTER19-TO PRINT THE SIMILAR ELEMENTS IN NXN MATRIX.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a[100][100],i,j,n,m;
+   scanf("%d",&n);
+   scanf("%d",&m);
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<m;j++)
+       {
+           scanf("%d",&a[i][j]);
+       }
+   }
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<m;j++)
+       {
+           if(a[i][j]==a[i+1][j])
+           {
+               printf("%d",a[i][j]);
+           }
+       }
+   }
+
+}
