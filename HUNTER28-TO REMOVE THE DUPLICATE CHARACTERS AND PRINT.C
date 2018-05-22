7feb9a32892c0de861822073266cf4ceaaafda92
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-05-22 22:32:25 +0530

    Create HUNTER28-TO REMOVE THE DUPLICATE CHARACTERS AND PRINT.C

---
+++ HUNTER28-TO REMOVE THE DUPLICATE CHARACTERS AND PRINT.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a[100],b[100];
+    int i,j,cnt=0,k=0;
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+      for(j=i+1;a[j]!='\0';j++)
+      {
+            if(a[i]==a[j])
+            {
+               a[j]='*';
+
+            }
+
+
+    }
+    }
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]!='*')
+        {
+            printf("%c",a[i]);
+        }
+    }
+
+}
+
